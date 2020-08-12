c0982a020a1cae6da0159be2052c33ef30ac9e74
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2020-08-12 10:49:27 -0500

    exercise 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Stats = ({good, neutral, bad}) => (
+  <div>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </div>
+)
+
+const Button = ({onClick, text}) => (
+  <button onClick = {onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+        <Button onClick={() => setGood(good + 1)} text="good"/>
+        <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+        <Button onClick={() => setBad(bad + 1)} text="bad"/>
+      <h1>statistics</h1>
+        <Stats good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
